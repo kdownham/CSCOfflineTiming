a6ea80aaf4380b7ac6e9a05af3b35458856fd576
fgolf <frank.golf@gmail.com>
2015-06-18 23:37:00 -0700

    add good run list

--- CSCTimingAnalyzer/test/test_cfg.py
+++ CSCTimingAnalyzer/test/test_cfg.py
@@ -2,6 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("CSCTIMINGANALYSIS")
 
+applyGoodRunList = cms.bool(True)
+
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
@@ -14,18 +16,27 @@ process.source = cms.Source("PoolSource",
                             )
 )
 
+import FWCore.PythonUtilities.LumiList as LumiList
+import FWCore.ParameterSet.Types as CfgTypes
+
+if applyGoodRunList:
+    process.source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
+    JSONfile = '/home/users/fgolf/csc/CMSSW_7_4_5_timing/src/CSCOfflineTiming/CSCTimingAnalyzer/json/Cert_246908-247381_13TeV_PromptReco_Collisions15_ZeroTesla_JSON.txt'
+    myLumis = LumiList.LumiList(filename = JSONfile).getCMSSWString().split(',')
+    process.source.lumisToProcess.extend(myLumis)
+
 process.load("CSCOfflineTiming.CSCTimingAnalyzer.cscTimingAnalyzer_cfi")
 process.cscTimingAnalyzer.makeChamberTimingCorrectionValueHists = cms.untracked.bool(True)
 process.cscTimingAnalyzer.makePlotsPerChamber = cms.bool(True)
-process.cscTimingAnalyzer.verbose = cms.untracked.bool(True)
+process.cscTimingAnalyzer.verbose = cms.untracked.bool(False)
 process.cscTimingAnalyzer.debug = cms.untracked.bool(False)
 process.cscTimingAnalyzer.applyUpdatedME11corrections = cms.bool(True)
 process.cscTimingAnalyzer.min_pt = cms.double(0)
 process.cscTimingAnalyzer.max_dz = cms.double(1)
 
-applyUpdatedME11correctionsName = 'updateME11corrections'
-min_pt_name = "minPt%d" & process.cscTimingAnalyzer.min_pt
-max_dz_name = "maxDz%d" & process.cscTimingAnalyzer.max_dz
+applyUpdatedME11correctionsName = '_updateME11corrections'
+# min_pt_name = '_minPt' + str(process.cscTimingAnalyzer.min_pt)
+# max_dz_name = '_maxDz' + str(process.cscTimingAnalyzer.max_dz)
 
 baseFileName = 'histos'
 fileExtension = '.root'
@@ -33,12 +44,13 @@ outfileName = baseFileName
 
 if process.cscTimingAnalyzer.applyUpdatedME11corrections:
     outfileName = outfileName + applyUpdatedME11correctionsName
-if process.cscTimingAnalyzer.min_pt > 1:
-    outfileName = outfileName + min_pt_name
-if process.cscTimingAnalyzer.max_dz != 0.5:
-    otufileName = outfileName + max_dz_name
+# if process.cscTimingAnalyzer.min_pt > 1:
+#     outfileName = outfileName + min_pt_name
+# if process.cscTimingAnalyzer.max_dz > 0.5:
+#     otufileName = outfileName + max_dz_name
 
 outfileName = outfileName + fileExtension
+print outfileName
 
 process.cscTimingAnalyzer.outfileName = cms.untracked.string(outfileName)
 
