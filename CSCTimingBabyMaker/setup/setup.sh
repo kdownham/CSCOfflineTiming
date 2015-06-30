740b88af35fca08f42128fcca652f04dc260fe9a
fgolf <frank.golf@gmail.com>
2015-06-29 18:38:43 -0700

    checkout RecoMuon/TrackingTools, needed to match CSC segments to SA muons using MuonSegmentMatcher

--- CSCTimingBabyMaker/setup/setup.sh
+++ CSCTimingBabyMaker/setup/setup.sh
@@ -16,3 +16,9 @@ sed -i '15d' ${CMSSW_BASE}/src/RecoLocalMuon/CSCRecHitD/BuildFile.xml
 echo "<export>" >> ${CMSSW_BASE}/src/RecoLocalMuon/CSCRecHitD/BuildFile.xml
 echo "   <lib   name=\"1\"/>" >> ${CMSSW_BASE}/src/RecoLocalMuon/CSCRecHitD/BuildFile.xml
 echo "</export>" >> ${CMSSW_BASE}/src/RecoLocalMuon/CSCRecHitD/BuildFile.xml
+
+if [ ! -f ${CMSSW_BASE}/src/RecoMuon/TrackingTools/BuildFile.xml ];
+then
+    cd ${CMSSW_BASE}/src
+    git cms-addpkg RecoMuon/TrackingTools
+fi
